56b916d7668428708a1a72420f61f3a59a2f643a
나 <xodnzlzl@naver.com>
2023-03-07 00:31:48 +0900

    LoginCheckAspect 수정

--- src/main/java/com/moida/aspect/LoginCheckAspect.java
+++ src/main/java/com/moida/aspect/LoginCheckAspect.java
@@ -1,16 +1,14 @@
 package com.moida.aspect;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.moida.exception.LoginRequiredException;
+import com.moida.request.FeedRequest;
 
 @Component
 @Aspect
@@ -24,10 +22,8 @@ public class LoginCheckAspect {
 
 	@Before("@annotation(com.moida.annotation.LoginCheck)")
 	public void checkLogin(JoinPoint joinPoint) {
-		ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
-		HttpServletRequest request = attributes.getRequest();
 
-		String userId = (String)session.getAttribute(request.getParameter("userId"));
+		String userId = (String)session.getAttribute(((FeedRequest)joinPoint.getArgs()[0]).getUserId());
 
 		if (userId == null) {
 			throw new LoginRequiredException();
